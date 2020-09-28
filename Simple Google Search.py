ac0e16aa16de422a1a961fb12a84941b6172e094
InMDev <48659591+InMDev@users.noreply.github.com>
2020-09-28 13:14:24 +0800

    Update and rename Bot1.py to Simple Google Search.py

--- Simple Google Search.py
+++ Simple Google Search.py
@@ -1,11 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+#Taking chromedriver.exe to launch chrome as the bot browser.
 PATH = "D:\DELL\Documents\Course\Python\Selenium\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
+#Take in "https://google.com" as the search bar
 driver.get("https://google.com")
 
+#Searching the search elements in google.com
 search = driver.find_element_by_name('q')
+
+#Inputs in the search bar
 search.send_keys("YAY IT CLICKS ")
+
+#Enters the RETURN key in the keyboard
 search.send_keys(Keys.RETURN)
